ddc3a0f955efcd0a0e7c3f93d3577e0e8b57a0de
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2025-05-06 10:50:33 -0300

    feat: calculadora component

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora',
+  imports: [ReactiveFormsModule],//responsavel por ativar o uso de formulario reativo
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+
+    resultado:number = 0
+    calculadoraForm : FormGroup;
+    constructor(private fb: FormBuilder) {
+      this.calculadoraForm = this.fb.group({
+        num1 : this.fb.control(50),
+        num2 : this.fb.control(30)
+      });
+    }
+
+
+
+    onCalcular(){
+      let num1 = 10;
+      let num2 = 20;
+      this.resultado = num1+num2;
+    }
+}
